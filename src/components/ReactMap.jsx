44abb5c9ef93a36a4da93a5576a65ca5bd217428
Saadat Taaseen <taaseen.homework@gmail.com>
2021-02-25 00:53:03 -0500

    added mapbox

--- src/components/ReactMap.jsx
+++ src/components/ReactMap.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactMapGL from "react-map-gl";
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl'
+
 
 function ReactMap() {
+
+    const [viewport, setViewport] = useState({
+        latitude: 45.4211,
+        longitude: -75.6903,
+        zoom: 9,
+        width: "95vw",
+        height: "50vh"
+    })
+
     return (
         <div>
-            MAP
+            <ReactMapGL {...viewport}>
+
+            </ReactMapGL>
         </div>
     )
 }
